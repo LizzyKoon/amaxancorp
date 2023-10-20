4079741126e3e909c712d98073834d6555832eef
Maxence Audibert <audibert.maxence@gmail.com>
2023-10-20 14:25:10 +0200

    m

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.assets.precompile += ['app.scss']
+  config.assets.precompile += ['app.css']
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
